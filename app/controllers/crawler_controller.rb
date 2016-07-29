3fc1891f1041fcfcbc33cbe159557e9af2d56198
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-07-29 11:17:37 -0400

    Avances

--- app/controllers/crawler_controller.rb
+++ app/controllers/crawler_controller.rb
@@ -7,34 +7,34 @@ class CrawlerController < ApplicationController
 	def poder_judicial_civil
 		require 'typhoeus'
 		require 'nokogiri'
-		result = Array.new
-		if params[:search].nil?
-			search = "colmena"
-		else
-			search = params[:search]
-		end
+		# result = Array.new
+		# if params[:search].nil?
+		# 	search = "colmena"
+		# else
+		# 	search = params[:search]
+		# end
 
 		###################################
 		### Obtiene header & session id ###
 		###################################
-		request1 = Typhoeus::Request.new(
-			"http://civil.poderjudicial.cl/CIVILPORWEB/",
-			method: :get
-		)
-		request1.run
-		headers = request1.response.headers.to_s
-		header_obj = JSON.parse headers.gsub('=>', ':')
-		session_id = header_obj['Set-Cookie'][0].split(';')[0].gsub('JSESSIONID=', '')
-		cookie = "FLG_Version=0; FLG_Turno=0; CRR_IdFuncionario=1; COD_TipoCargo=2; COD_Tribunal=0; COD_Corte=98; COD_Usuario=autoconsulta1; GLS_Tribunal=Tribunal de Prueba 1; GLS_Comuna=Santiago; COD_Ambiente=3; COD_Aplicacion=2; GLS_Usuario=Juan Pena Perez; HORA_LOGIN=08:46; _ga=GA1.2.1541246786.1468964940; "+header_obj['Set-Cookie'][0].split(';')[0]+"+zwasportal11"
-		cookie = "FLG_Version=0; FLG_Turno=0; CRR_IdFuncionario=1; COD_TipoCargo=2; COD_Tribunal=0; COD_Corte=98; COD_Usuario=autoconsulta1; GLS_Tribunal=Tribunal de Prueba 1; GLS_Comuna=Santiago; COD_Ambiente=3; COD_Aplicacion=2; GLS_Usuario=Juan Peña Perez; HORA_LOGIN=08:46; _ga=GA1.2.1541246786.1468964940; JSESSIONID=0003zft1cZ2IAVhVP0fJjhdI3RL+zwasportal12+zwasportal11"
+		# request1 = Typhoeus::Request.new(
+		# 	"http://civil.poderjudicial.cl/CIVILPORWEB/",
+		# 	method: :get
+		# )
+		# request1.run
+		# headers = request1.response.headers.to_s
+		# header_obj = JSON.parse headers.gsub('=>', ':')
+		# session_id = header_obj['Set-Cookie'][0].split(';')[0].gsub('JSESSIONID=', '')
+		# cookie = "FLG_Version=0; FLG_Turno=0; CRR_IdFuncionario=1; COD_TipoCargo=2; COD_Tribunal=0; COD_Corte=98; COD_Usuario=autoconsulta1; GLS_Tribunal=Tribunal de Prueba 1; GLS_Comuna=Santiago; COD_Ambiente=3; COD_Aplicacion=2; GLS_Usuario=Juan Pena Perez; HORA_LOGIN=08:46; _ga=GA1.2.1541246786.1468964940; "+header_obj['Set-Cookie'][0].split(';')[0]+"+zwasportal11"
+		# cookie = "FLG_Version=0; FLG_Turno=0; CRR_IdFuncionario=1; COD_TipoCargo=2; COD_Tribunal=0; COD_Corte=98; COD_Usuario=autoconsulta1; GLS_Tribunal=Tribunal de Prueba 1; GLS_Comuna=Santiago; COD_Ambiente=3; COD_Aplicacion=2; GLS_Usuario=Juan Peña Perez; HORA_LOGIN=08:46; _ga=GA1.2.1541246786.1468964940; JSESSIONID=0003zft1cZ2IAVhVP0fJjhdI3RL+zwasportal12+zwasportal11"
 
-		"0001O-mN_As4AZoOOANEyagZXlX+zwasportal10"
-		"00014E7BK2DjPEzHFt-rABrHOiK+zwasportal10"
+		# "0001O-mN_As4AZoOOANEyagZXlX+zwasportal10"
+		# "00014E7BK2DjPEzHFt-rABrHOiK+zwasportal10"
 
-		puts "- DEBUG -"
-		puts session_id
-		puts "---"
-		puts cookie
+		# puts "- DEBUG -"
+		# puts session_id
+		# puts "---"
+		# puts cookie
 
 
 
@@ -95,26 +95,32 @@ class CrawlerController < ApplicationController
 		# puts "- DEBUG -"
 		# puts request2.response.body
 		
-		body = Nokogiri::HTML(search_request("COLMENA", "0003zft1cZ2IAVhVP0fJjhdI3RL+zwasportal12+zwasportal11"))
-		row = body.css('#contentCellsAddTabla tr')
-		row.each do |obj|
-			data = {
-				rol: obj.css('td')[0].text.squish,
-				date: obj.css('td')[1].text.squish,
-				caption: obj.css('td')[2].text.squish,
-				court: obj.css('td')[3].text.squish
-			}
-			# Creamos un registro en la BD.
-			caso = Case.new(data)
-			caso.save
-			puts "- DEBUG Data -"
-			puts data
-			result << data
-		end
-		if result.any?
-			render :json => { :status => true, :message => "Se encontraron las siguientes causas.", :result => result }, :status => 200
+		#body = Nokogiri::HTML(search_request_civil("COLMENA", "0003zft1cZ2IAVhVP0fJjhdI3RL+zwasportal12+zwasportal11"))
+		body = Nokogiri::HTML(search_request_corte("COLMENA", "0000yBl_ImwapLSUjDnNwvZPZwL:-1"))
+		if !body.empty?
+			row = body.css('#contentCellsAddTabla tr')
+			row.each do |obj|
+				puts obj
+				# data = {
+				# 	rol: obj.css('td')[0].text.squish,
+				# 	date: obj.css('td')[1].text.squish,
+				# 	caption: obj.css('td')[2].text.squish,
+				# 	court: obj.css('td')[3].text.squish
+				# }
+				# # Creamos un registro en la BD.
+				# caso = Case.new(data)
+				# caso.save
+				# puts "- DEBUG Data -"
+				# puts data
+				# result << data
+			end
+			if result.empty?
+				render :json => { :status => true, :message => "Se encontraron las siguientes causas.", :result => result }, :status => 200
+			else
+				render :json => { :status => false, :message => "No se encontraron causas.", :result => result }, :status => 200
+			end
 		else
-			render :json => { :status => false, :message => "No se encontraron causas.", :result => result }, :status => 200
+			render :json => { :status => false, :message => "No respondio nada.", :result => result }, :status => 200
 		end
 	end
 
@@ -122,9 +128,76 @@ class CrawlerController < ApplicationController
 		@cases = Case.all
 	end
 
+	def search_request_corte(key, jsessionid)
+		require 'net/http'
+		begin
+			uri = URI('http://corte.poderjudicial.cl/SITCORTEPORWEB/AtPublicoDAction.do')
+			# Create client
+			http = Net::HTTP.new(uri.host, uri.port)
+			data = {
+				"TIP_Consulta" => "3",
+				"TIP_Lengueta" => "tdNombre",
+				"APE_Paterno" => "",
+				"TIP_Causa" => "+",
+				"RUC_Tribunal" => "",
+				"RUC_Numero" => "",
+				"COD_Corte" => "0",
+				"ERA_Recurso" => "",
+				"COD_LibroCmb" => "",
+				"FEC_Desde" => "28/07/2016",
+				"NOM_Consulta" => key,
+				"irAccionAtPublico" => "Consulta",
+				"COD_Libro" => "null",
+				"APE_Materno" => "",
+				"ROL_Recurso" => "",
+				"ROL_Causa" => "",
+				"FEC_Hasta" => "28/07/2016",
+				"RUC_Dv" => "",
+				"selConsulta" => "0",
+				"ERA_Causa" => "",
+				"RUC_Era" => "",
+			}
+			body = URI.encode_www_form(data)
+			# Create Request
+			req =  Net::HTTP::Post.new(uri)
+			# Add headers
+			req.add_field "Connection", "keep-alive"
+			# Add headers
+			req.add_field "Accept-Encoding", "gzip, deflate"
+			# Add headers
+			req.add_field "Upgrade-Insecure-Requests", "1"
+			# Add headers
+			req.add_field "Content-Type", "application/x-www-form-urlencoded"
+			# Add headers
+			req.add_field "Origin", "http://corte.poderjudicial.cl"
+			# Add headers
+			req.add_field "Cache-Control", "no-cache"
+			# Add headers
+			req.add_field "User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36"
+			# Add headers
+			req.add_field "Cookie", "CRR_IdFuncionario=0; COD_TipoCargo=0; COD_Corte=90; COD_Usuario=autoconsulta; GLS_Corte=C.A. de Santiago; COD_Ambiente=3; COD_Aplicacion=3; GLS_Usuario=; HORA_LOGIN=12:30; NUM_SalaUsuario=0; JSESSIONID=#{jsessionid}; _ga=GA1.2.1541246786.1468964940; _gat=1;"
+			# Add headers
+			req.add_field "Referer", "http://corte.poderjudicial.cl/SITCORTEPORWEB/AtPublicoViewAccion.do?tipoMenuATP=1"
+			# Add headers
+			req.add_field "Pragma", "no-cache"
+			# Add headers
+			req.add_field "Accept-Language", "es,en;q=0.8"
+			# Add headers
+			req.add_field "Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8"
+			# Set header and body
+			req.body = body
+			# Fetch Request
+			res = http.request(req)
+			puts "Response HTTP Status Code: #{res.code}"
+			#puts "Response HTTP Response Body: #{res.body}"
+			return res.body
+		rescue StandardError => e
+			puts "HTTP Request failed (#{e.message})"
+			return false
+		end
+	end
 
-
-	def search_request(key, jsessionid)
+	def search_request_civil(key, jsessionid)
 		require 'net/http'
 		begin
 			uri = URI('http://civil.poderjudicial.cl/CIVILPORWEB/AtPublicoDAction.do')
@@ -178,10 +251,11 @@ class CrawlerController < ApplicationController
 			# Fetch Request
 			res = http.request(req)
 			puts "Response HTTP Status Code: #{res.code}"
-			return res.body
 			#puts "Response HTTP Response Body: #{res.body}"
+			return res.body
 		rescue StandardError => e
 			puts "HTTP Request failed (#{e.message})"
+			return false
 		end
 	end
 
