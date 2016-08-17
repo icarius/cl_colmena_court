7c7c582f7af8a22f803f49f1389cf723bd962a09
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-08-17 18:08:01 -0300

    Fix model

--- db/migrate/20160817210326_remove_estado_colmena_procesalfrom_case.rb
+++ db/migrate/20160817210326_remove_estado_colmena_procesalfrom_case.rb
@@ -1,5 +1,7 @@
 class RemoveEstadoColmenaProcesalfromCase < ActiveRecord::Migration[5.0]
 	def change
-		remove_column :cases, :estado_colmena_procesal
+		unless table_exists? :cases, :estado_colmena_procesal
+			remove_column :cases, :estado_colmena_procesal
+		end
 	end
 end
