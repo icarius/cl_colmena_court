b11fdd8631cccf375f4a41575d36e579c073ff0c
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-08-17 23:32:42 -0300

    Update cron

--- config/schedule.rb
+++ config/schedule.rb
@@ -18,6 +18,6 @@
 # end
 
 # Learn more: http://github.com/javan/whenever
-# every 1.day, :at => '3:30 am' do
-# 	runner "Case.poderjudicial_crawler('COLMENA')"
-# end
+every 1.day, :at => '11:59 pm' do
+	runner "Case.poderjudicial_crawler('COLMENA')"
+end
