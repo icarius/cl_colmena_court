fbc1f1ba7a087ee51fcfe5a5dbc02f2878164ea2
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-10-17 22:50:52 -0300

    fix change circuit

--- app/controllers/case_controller.rb
+++ app/controllers/case_controller.rb
@@ -95,7 +95,7 @@ class CaseController < ApplicationController
 		require 'net/telnet'
 		agenta = TorPrivoxy::Agent.new '66.175.216.65', '', 8118 => 9051
 		oldip = agenta.ip
-		localhost = Net::Telnet::new("Host" => "localhost", "Port" => "9051", "Timeout" => 10, "Prompt" => /250 OK\n/)
+		localhost = Net::Telnet::new("Host" => "66.175.216.65", "Port" => "9051", "Timeout" => 10, "Prompt" => /250 OK\n/)
 		localhost.cmd('AUTHENTICATE ""') { |c| print c; throw "Cannot authenticate to Tor" if c != "250 OK\n" }
 		localhost.cmd('signal NEWNYM') { |c| print c; throw "Cannot switch Tor to new route" if c != "250 OK\n" }
 		localhost.close
