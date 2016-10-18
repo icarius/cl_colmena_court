16ae79edb03aee60bc0ac1cc9ac8e431f6f19209
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-10-17 21:14:17 -0300

    fix change circuit

--- app/models/case.rb
+++ app/models/case.rb
@@ -248,11 +248,11 @@ class Case < ApplicationRecord
 	end
 
 	def self.switch_tor_circuit
-		require 'net/telnet'
-		localhost = Net::Telnet::new("Host" => "localhost", "Port" => "9051", "Timeout" => 10, "Prompt" => /250 OK\n/)
-		localhost.cmd('AUTHENTICATE ""') { |c| print c; throw "Cannot authenticate to Tor" if c != "250 OK\n" }
-		localhost.cmd('signal NEWNYM') { |c| print c; throw "Cannot switch Tor to new route" if c != "250 OK\n" }
-		localhost.close
+		# require 'net/telnet'
+		# localhost = Net::Telnet::new("Host" => "localhost", "Port" => "9051", "Timeout" => 10, "Prompt" => /250 OK\n/)
+		# localhost.cmd('AUTHENTICATE ""') { |c| print c; throw "Cannot authenticate to Tor" if c != "250 OK\n" }
+		# localhost.cmd('signal NEWNYM') { |c| print c; throw "Cannot switch Tor to new route" if c != "250 OK\n" }
+		# localhost.close
 	end
 
 	def self.send_request_court_mechanize(jsessionid, search)
