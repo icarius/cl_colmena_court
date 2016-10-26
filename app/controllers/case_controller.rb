d2d5027cfa76dddea5328478ccb8bbbabda74dce
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2016-10-26 18:57:05 -0300

    Avances y RC disponible

--- app/controllers/case_controller.rb
+++ app/controllers/case_controller.rb
@@ -70,6 +70,42 @@ class CaseController < ApplicationController
 		render :json => { :status => true, :estado_colmena_situacion => params[:estado_colmena_situacion] }, :status => 200
 	end
 
+	def newsovercases
+		@news = Case.poderjudicial_news_cases_details
+	end
+
+	def daily_mail_report
+		now_date_time = DateTime.now
+		destinatarios = [
+			{
+				:name => 'Felipe I. González G.',
+				:email => 'felipe@coddea.com'
+			}
+		]
+		# Mail 1
+		title1 = 'Nuevas causas '+now_date_time.strftime('%d-%m-%Y')
+		@cases = Case.poderjudicial_new_cases
+		msj1_txt = ""
+		msj1_html = render_to_string partial: '/shared/mail/news', :locals => {:@title => title1}, :object => @cases, layout: false
+		Case.send_email(
+			title1,
+			destinatarios,
+			msj1_txt,
+			msj1_html
+		)
+		# Mail 2
+		title2 = 'Novedades '+now_date_time.strftime('%d-%m-%Y')
+		@elements = Case.poderjudicial_news_cases_details
+		msj2_html = render_to_string partial: '/shared/mail/developments', :locals => {:@title => title2}, :object => @elements, layout: false
+		Case.send_email(
+			title2,
+			destinatarios,
+			msj1_txt,
+			msj2_html
+		)
+		render :json => { :status => true }, :status => 200
+	end
+
 	def study_crawler
 		result = StudyCase.study_crawler(params[:search])
 		render :json => { :status => true, :study_crawler_search => params[:search], :study_crawler_result => result }, :status => 200
@@ -103,39 +139,7 @@ class CaseController < ApplicationController
 		newip = agentb.ip
 		render :json => { :status => true, :oldip => oldip, :newip => newip}, :status => 200
 	end
-
-	def mail_test
-		now_date_time = DateTime.now
-		destinatarios = [
-			{
-				:name => 'Felipe I. González G.',
-				:email => 'felipe@coddea.com'
-			}
-		]
-		# Mail 1
-		title1 = 'Nuevas causas '+now_date_time.strftime('%d-%m-%Y')
-		@cases = Case.poderjudicial_new_cases
-		msj1_txt = ""
-		msj1_html = render_to_string partial: '/shared/mail/news', :locals => {:@title => title1}, :object => @cases, layout: false
-		Case.send_email(
-			title1,
-			destinatarios,
-			msj1_txt,
-			msj1_html
-		)
-		# Mail 2
-		title2 = 'Novedades '+now_date_time.strftime('%d-%m-%Y')
-		@elements = Case.poderjudicial_news_cases_details
-		msj2_html = render_to_string partial: '/shared/mail/developments', :locals => {:@title => title2}, :object => @elements, layout: false
-		Case.send_email(
-			title2,
-			destinatarios,
-			msj1_txt,
-			msj2_html
-		)
-		render :json => { :status => true }, :status => 200
-	end
-
+ 
 	def news_test
 		Case.poderjudicial_news_crawler
 		render :json => { :status => true }, :status => 200
--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,11 @@ Rails.application.routes.draw do
 	root to: "case#dashboard"
 
 	devise_for :users
-	
-	# scope '/api' do
-	# 	scope '/v1' do
-	# 		# Crawler test
-	# 		scope '/case' do
-	# 			get '/test' => 'case#test'
-	# 			get '/test/:search' => 'case#test'
-	# 		end
-	# 	end
-	# end
 
+	scope '/newsovercases' do
+		get '/' => 'case#newsovercases'
+	end
+	
 	scope '/api' do
 		scope '/estado' do
 			get '/:id_caso/:estado_colmena' => 'case#update_estado'
@@ -24,12 +18,14 @@ Rails.application.routes.draw do
 		scope '/study' do
 			get '/crawler/:search' => 'case#study_crawler'
 		end
+		scope '/reporte' do
+			get '/mail' => 'case#daily_mail_report'
+		end
 		scope '/test' do
 			get '/proxy' => 'case#proxy_test'
 			get '/crawler' => 'case#crawler_test'
 			get '/driver' => 'case#driver_test'
 			get '/circuit' => 'case#circuit_test'
-			get '/mail' => 'case#mail_test'
 			get '/news' => 'case#news_test'
 			get '/getnews' => 'case#get_news_test'
 		end
