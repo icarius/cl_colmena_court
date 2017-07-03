3eac5f6cb1ab5ee4a9a82546982071f65640edb9
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2017-07-03 10:53:13 -0400

    Fix ble

--- app/workers/daily_mail_report_worker.rb
+++ app/workers/daily_mail_report_worker.rb
@@ -1,7 +1,6 @@
 class DailyMailReportWorker
 	include Sidekiq::Worker
 	def perform
-		ac = ActionController::Base.new()
 		now_date_time = DateTime.now
 		destinatarios = [
 			{
@@ -13,7 +12,7 @@ class DailyMailReportWorker
 		title1 = 'Nuevas causas '+now_date_time.strftime('%d-%m-%Y')
 		@cases = Case.poderjudicial_new_cases
 		msj1_txt = ""
-		msj1_html = ac.render_to_string partial: '/shared/mail/news', :locals => {:@title => title1}, :object => @cases, layout: false
+		msj1_html = CaseController.new.render_to_string partial: '/shared/mail/news', :locals => {:@title => title1}, :object => @cases, layout: false
 		Case.send_email(
 			title1,
 			destinatarios,
@@ -23,7 +22,7 @@ class DailyMailReportWorker
 		# Mail 2
 		title2 = 'Novedades '+now_date_time.strftime('%d-%m-%Y')
 		@elements = Case.poderjudicial_news_cases_details
-		msj2_html = ac.render_to_string partial: '/shared/mail/developments', :locals => {:@title => title2}, :object => @elements, layout: false
+		msj2_html = CaseController.new.render_to_string partial: '/shared/mail/developments', :locals => {:@title => title2}, :object => @elements, layout: false
 		Case.send_email(
 			title2,
 			destinatarios,
