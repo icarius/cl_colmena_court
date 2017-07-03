c33d56d4a96e77566b7661ffad819304732b861a
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2017-07-03 10:47:56 -0400

    Fix

--- app/workers/daily_mail_report_worker.rb
+++ app/workers/daily_mail_report_worker.rb
@@ -1,6 +1,7 @@
 class DailyMailReportWorker
 	include Sidekiq::Worker
 	def perform
+		ac = ActionController::Base.new()
 		now_date_time = DateTime.now
 		destinatarios = [
 			{
@@ -12,7 +13,7 @@ class DailyMailReportWorker
 		title1 = 'Nuevas causas '+now_date_time.strftime('%d-%m-%Y')
 		@cases = Case.poderjudicial_new_cases
 		msj1_txt = ""
-		msj1_html = render_to_string partial: '/shared/mail/news', :locals => {:@title => title1}, :object => @cases, layout: false
+		msj1_html = ac.render_to_string partial: '/shared/mail/news', :locals => {:@title => title1}, :object => @cases, layout: false
 		Case.send_email(
 			title1,
 			destinatarios,
@@ -22,13 +23,12 @@ class DailyMailReportWorker
 		# Mail 2
 		title2 = 'Novedades '+now_date_time.strftime('%d-%m-%Y')
 		@elements = Case.poderjudicial_news_cases_details
-		msj2_html = render_to_string partial: '/shared/mail/developments', :locals => {:@title => title2}, :object => @elements, layout: false
+		msj2_html = ac.render_to_string partial: '/shared/mail/developments', :locals => {:@title => title2}, :object => @elements, layout: false
 		Case.send_email(
 			title2,
 			destinatarios,
 			msj1_txt,
 			msj2_html
 		)
-		# render :json => { :status => true }, :status => 200
 	end
 end
