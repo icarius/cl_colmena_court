c1c92e17c95438dea9d790a177805bc6d944d208
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2017-07-03 14:40:12 -0400

    Fix ble

--- app/controllers/case_controller.rb
+++ app/controllers/case_controller.rb
@@ -118,6 +118,38 @@ class CaseController < ApplicationController
 		render :json => { :status => true }, :status => 200
 	end
 
+	def daily_mail_report_fc
+		now_date_time = DateTime.now
+		destinatarios = [
+			{
+				:name => 'Felipe I. González G.',
+				:email => 'felipe@coddea.com'
+			}
+		]
+		# Mail 1
+		title1 = 'Nuevas causas '+now_date_time.strftime('%d-%m-%Y')
+		@cases = Case.poderjudicial_new_cases
+		msj1_txt = ""
+		msj1_html = render_to_string partial: '/shared/mail/news', :locals => {:@title => title1}, :object => @cases, layout: false
+		Case.send_email(
+			title1,
+			destinatarios,
+			msj1_txt,
+			msj1_html
+		)
+		# Mail 2
+		title2 = 'Novedades '+now_date_time.strftime('%d-%m-%Y')
+		@elements = Case.poderjudicial_news_cases_details
+		msj2_html = render_to_string partial: '/shared/mail/developments', :locals => {:@title => title2}, :object => @elements, layout: false
+		Case.send_email(
+			title2,
+			destinatarios,
+			msj1_txt,
+			msj2_html
+		)
+		return true
+	end
+
 	def study_crawler
 		result = StudyCase.study_crawler(params[:search])
 		render :json => { :status => true, :study_crawler_search => params[:search], :study_crawler_result => result }, :status => 200
--- app/workers/daily_mail_report_worker.rb
+++ app/workers/daily_mail_report_worker.rb
@@ -1,6 +1,6 @@
 class DailyMailReportWorker
 	include Sidekiq::Worker
 	def perform
-		CaseController.new.daily_mail_report
+		CaseController.new.daily_mail_report_fc
 	end
 end
