11d93eaf2d15925316a4b1e2759d137f5662a5ae
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2017-11-18 16:41:22 -0300

    Fix

--- app/models/case.rb
+++ app/models/case.rb
@@ -303,7 +303,7 @@ class Case < ApplicationRecord
 		puts origin
 		require 'net/telnet'
 		localhost = Net::Telnet::new("Host" => "127.0.0.1", "Port" => "9051", "Timeout" => 10, "Prompt" => /250 OK\n/)
-		localhost.cmd('AUTHENTICATE "colmena"') { |c| print c; throw "Cannot authenticate to Tor" if c != "250 OK\n" }
+		localhost.cmd('AUTHENTICATE "reframe2015+-"') { |c| print c; throw "Cannot authenticate to Tor" if c != "250 OK\n" }
 		localhost.cmd('signal NEWNYM') { |c| print c; throw "Cannot switch Tor to new route" if c != "250 OK\n" }
 		localhost.close
 	end
