a790c9e4b659a79c23df292946edc1aa8f587a66
Felipe I. González G <felipe.gonzalez.g@gmail.com>
2018-03-09 19:56:52 -0300

    Test fix

--- app/models/case.rb
+++ app/models/case.rb
@@ -337,7 +337,7 @@ class Case < ApplicationRecord
 		require 'selenium-webdriver'
 		# Creo el driver para obtener la session y poder ejecutar el request.
 		# driver = Selenium::WebDriver.for :phantomjs, args: '--proxy=localhost:8118 --proxy-type=socks5'
-		driver = Selenium::WebDriver.for :phantomjs, args: '--proxy=127.0.0.1:8118'
+		driver = Selenium::WebDriver.for :phantomjs, args: ['--proxy=127.0.0.1:8118', '--proxy-type=socks5']
 		driver.navigate.to "http://corte.poderjudicial.cl/SITCORTEPORWEB/"
 		# Obtengo el valor de JSESSIONID.
 		cookie = driver.manage.cookie_named("JSESSIONID")
